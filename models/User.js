7f5a906ad9cc23e5d21746e68d81228b8b7312de
Vermont Garcia <vermontgarcia@hotmail.com>
2020-02-15 08:45:04 -0600

    Department and position added to User schema

--- models/User.js
+++ models/User.js
@@ -4,8 +4,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  username:{
+  name:{
     type: String
+  },
+  department:{
+    type: String,
+  },
+  position:{
+    type: String,
   },
   email:{
     type: String,
