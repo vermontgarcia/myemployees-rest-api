b7ca8dd74578c1759b689ea468fb824d4be7c790
Vermont Garcia <vermontgarcia@hotmail.com>
2020-02-15 23:46:42 -0600

    GET Employees list route implemented

--- routes/employees.js
+++ routes/employees.js
@@ -3,8 +3,20 @@ const Employee = require('../models/User')
 
 const employeeRouter = express.Router();
 
+employeeRouter.get('/list', (req, res) => {
 
+  // Get the list of all employees
 
-
+  Employee.find()
+    .then(list=>{
+      list.forEach(employee => {
+        delete employee._doc.password
+      })
+      res.status(200).json({list, msg: 'List retrieved succesfully'});
+    })
+    .catch(err=>{
+      res.status(500).json({err, msg:'Something went grong. List not retrieved'})
+    });
+});
 
 module.exports = employeeRouter;
