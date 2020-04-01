7a6e8a064827ce85c402602bd5f8bac5e8875697
Vermont Garcia <vermontgarcia@hotmail.com>
2020-04-01 12:54:30 -0400

    Minor fixes to front end

---
+++ public/precache-manifest.665e7c91828bdfba3c202cd1414bdbad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b694a57991079b48ced30c8729b8df8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c37e41d6221ea89a0b3e",
+    "url": "/static/css/2.9ddb28ea.chunk.css"
+  },
+  {
+    "revision": "a1a9ef2e170f614cffd2",
+    "url": "/static/css/main.176d08ed.chunk.css"
+  },
+  {
+    "revision": "c37e41d6221ea89a0b3e",
+    "url": "/static/js/2.5f2fec17.chunk.js"
+  },
+  {
+    "revision": "77c52c67682ebaf3c87f021fd687577f",
+    "url": "/static/js/2.5f2fec17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1a9ef2e170f614cffd2",
+    "url": "/static/js/main.4e0b5997.chunk.js"
+  },
+  {
+    "revision": "fbef83d38ba4914b5ee9",
+    "url": "/static/js/runtime-main.4c5b06f5.js"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2babfbc679784c294da5010a02e06ad3.js"
+  "/precache-manifest.665e7c91828bdfba3c202cd1414bdbad.js"
 );
 
 self.addEventListener('message', (event) => {
